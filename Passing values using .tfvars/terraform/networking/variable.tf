0a329a7989826b1bed50926aad8ca11c7c28bcca
summu97 <135579543+summu97@users.noreply.github.com>
2024-04-05 14:53:50 +0530

    Create variable.tf

---
+++ Passing values using .tfvars/terraform/networking/variable.tf
@@ -0,0 +1,12 @@
+variable "project_id" {}
+variable "vpc_name" {}
+variable "bastion_subnet" {}
+variable "bastion_region" {}
+variable "bastion_cidr" {}
+variable "private_subnet" {}
+variable "private_region" {}
+variable "private_cidr" {}
+variable "bastion_firewall_name" {}
+variable "bastion_network_tags" {}
+variable "private_firewall_name" {}
+variable "private_network_tags" {}
