739dce20a93647fb1a7d9db80717c890e3dc0068
summu97 <135579543+summu97@users.noreply.github.com>
2024-04-05 14:50:41 +0530

    Update main.tf

--- Passing values using .tfvars/terraform/main.tf
+++ Passing values using .tfvars/terraform/main.tf
@@ -1,1 +1,21 @@
+provider "google" {
+  project = "ringed-hallway-417305"
+  region  = "us-east1-b"
+}
 
+module "networking" {
+  source    = "./networking"
+
+  project_id            = var.project_id
+  vpc_name              = var.vpc_name
+  bastion_subnet        = var.bastion_subnet
+  bastion_region        = var.bastion_region
+  bastion_cidr          = var.bastion_cidr
+  private_subnet        = var.private_subnet
+  private_region        = var.private_region
+  private_cidr          = var.private_cidr
+  bastion_firewall_name = var.bastion_firewall_name
+  bastion_network_tags  = var.bastion_network_tags
+  private_firewall_name = var.private_firewall_name
+  private_network_tags  = var.private_network_tags
+}
